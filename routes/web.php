13d7cce5f3a07471aab0f8431b768ba1a385cf73
abdurrahmangrahadimaulana <abdurrahmangrahadimaulana@gmail.com>
2023-01-09 04:55:16 +0700

    clean code

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,6 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\RiwayatController;
 use App\Http\Controllers\Admin\SettingWebsiteController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::group(['middleware' => ['xss']], function () {
     Route::controller(FrontendController::class)->name('fe.')->group(function () {
         Route::get('/', 'index')->name('index');
